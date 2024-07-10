16db5ae18a0ac4a6a56a9b8de1213686b3693415
dewnavapan <dewnavapan1@gmail.com>
2024-07-10 23:20:38 +0700

    create

---
+++ apqp/apqp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace apqp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
